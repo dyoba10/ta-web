cc9d6794ca8e27c479d32a73400bb0f12ca33952
Israq Dyoba <israqdyoba@gmail.com>
2021-06-15 01:30:03 +0700

    git ignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '2554e0b989210d428fe6856a54a866d48ce15977',
+        'reference' => 'd0364829b37c6bf2324aa1b72309091f77d55a35',
         'name' => 'codeigniter/framework',
         'dev' => true,
     ),
@@ -16,7 +16,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '2554e0b989210d428fe6856a54a866d48ce15977',
+            'reference' => 'd0364829b37c6bf2324aa1b72309091f77d55a35',
             'dev_requirement' => false,
         ),
         'doctrine/instantiator' => array(
