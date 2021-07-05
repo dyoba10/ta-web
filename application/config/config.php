f75b047edb4ce2caaee1a0cb068c3a97eccb3d16
Israq Dyoba <israqdyoba@gmail.com>
2021-07-06 03:14:59 +0700

    setting for push heroku

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://localhost/ta-web/';
+$config['base_url'] = 'https://sig-wisata.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
